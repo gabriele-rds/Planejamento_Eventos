33fe2aa5ef98f1589d17b8ff4eaf2084458e8561
Gabriele <gabrielegrs.16@gmail.com>
2024-04-14 17:31:11 -0400

    commit

--- Assets/pages/Assets/js/script.js
+++ Assets/pages/Assets/js/script.js
@@ -1,26 +1,39 @@
 // Função para calcular o total do orçamento
 function calcularTotal() {
-    let total = 0;
-  
-    // Adiciona o valor de cada serviço selecionado ao total
-    if (document.getElementById('fotografia').checked) {
-      total += parseInt(document.getElementById('fotografia').value);
-    }
-    if (document.getElementById('dj').checked) {
-      total += parseInt(document.getElementById('dj').value);
-    }
-    if (document.getElementById('decoracao').checked) {
-      total += parseInt(document.getElementById('decoracao').value);
-    }
-  
-    // Exibe o total na página
-    document.getElementById('total').textContent = total;
+  let total = 0;
+
+  // Adiciona o valor de cada serviço selecionado ao total
+  if (document.getElementById('fotografia').checked) {
+    total += parseInt(document.getElementById('fotografia').value);
   }
-  
-  // Adiciona um listener de mudança para calcular o total sempre que uma seleção for alterada
-  document.querySelectorAll('input[type="checkbox"]').forEach(input => {
-    input.addEventListener('change', calcularTotal);
-  });
+  if (document.getElementById('dj').checked) {
+    total += parseInt(document.getElementById('dj').value);
+  }
+  if (document.getElementById('decoracao').checked) {
+    total += parseInt(document.getElementById('decoracao').value);
+  }
+  if (document.getElementById('flores').checked) {
+    total += parseInt(document.getElementById('flores').value);
+  }
+  if (document.getElementById('aluguelLocal').checked) {
+    total += parseInt(document.getElementById('aluguelLocal').value);
+  }
+  if (document.getElementById('convite').checked) {
+    total += parseInt(document.getElementById('convite').value);
+  }
+  if (document.getElementById('buffet').checked) {
+    total += parseInt(document.getElementById('buffet').value);
+  }
+
+  // Exibe o total na página
+  document.getElementById('total').textContent = total;
+}
+
+// Adiciona um listener de mudança para calcular o total sempre que uma seleção for alterada
+document.querySelectorAll('input[type="checkbox"]').forEach(input => {
+  input.addEventListener('change', calcularTotal);
+});
+
 
   lightbox.option({
     'resizeDuration': 200,
